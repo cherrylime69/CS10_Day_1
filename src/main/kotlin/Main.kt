c0c7ba0ebb9d4a1d9c812eb7c323dca2d636256f
cherrylime69 <zerolecture0922@gmail.com>
2022-01-03 16:15:38 +0900

    논리 게이트 함수 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main(args: Array<String>) {
-    println("시작!")
+
 }
+
+//논리 게이트 역할을 하는 함수
+fun and(a:Boolean, b:Boolean) = (a) && (b)
+
+fun or(a:Boolean, b:Boolean) = (a) || (b)
+
+fun nand(a:Boolean, b:Boolean) = !((a) && (b))
+
+fun xor(a:Boolean, b:Boolean) = !((a) || (b))
